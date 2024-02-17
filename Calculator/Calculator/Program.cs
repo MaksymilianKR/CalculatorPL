beebb249afc43bfd2a63d9788d7f361d63a47af8
MaksymilianKR <maksymilian.official@gmail.com>
2024-02-17 20:37:04 +0100

    dodanie koncowej wersji

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -12,25 +12,19 @@ namespace Calculator
                 Console.WriteLine("Witaj w Aplikalcji KALULATOR!");
                 Console.WriteLine("Wpisz dowolną liczbę:");
 
-                //pobranie liczby od użytkownika
                 var number1 = int.Parse(Console.ReadLine());
 
 
-                //Poproś o podanie działania
                 Console.WriteLine("Co chcesz zrobić? możliwe opacje to: '+' '-' '*' '/'");
 
-                //pobieranie wybranego działania od użytkownika
                 var operation = Console.ReadLine();
 
-                //prośba o podanie drugiej liczby
                 Console.WriteLine("Wpisz proszę drugą liczbę:");
 
-                //pobranie liczby użytkownika
                 var number2 = int.Parse(Console.ReadLine());
 
                 var result = 0;
 
-                //Wykonanie obliczeń
                 switch (operation)
                 {
                     case "+":
@@ -50,12 +44,10 @@ namespace Calculator
 
                 }
 
-                //Wyświetlenie wyniku użytkownikowi '
                 Console.WriteLine($"Wynik twojego działania to: {result}");
             }
             catch (Exception ex)
             {
-                //logowanie do pliku informacji o błędzie
                 Console.WriteLine(ex.Message);
             }
 
